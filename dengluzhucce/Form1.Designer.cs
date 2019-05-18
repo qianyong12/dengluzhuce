84f419b56f565b39f1d37a9cc70245adc4529e63
1965398790 <1965398790@qq.com>
2019-05-18 08:19:15 +0800

    添加项目文件。

---
+++ dengluzhucce/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace dengluzhucce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.textPwd = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.UserPwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "注册";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(529, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "登录";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(132, 82);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(152, 25);
+            this.textUser.TabIndex = 2;
+            // 
+            // textPwd
+            // 
+            this.textPwd.Location = new System.Drawing.Point(131, 131);
+            this.textPwd.Name = "textPwd";
+            this.textPwd.Size = new System.Drawing.Size(153, 25);
+            this.textPwd.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(130, 182);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 25);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(128, 232);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(155, 25);
+            this.textEmail.TabIndex = 5;
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(522, 108);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(143, 25);
+            this.Username.TabIndex = 6;
+            this.Username.Text = "admin";
+            // 
+            // UserPwd
+            // 
+            this.UserPwd.Location = new System.Drawing.Point(522, 179);
+            this.UserPwd.Name = "UserPwd";
+            this.UserPwd.Size = new System.Drawing.Size(143, 25);
+            this.UserPwd.TabIndex = 7;
+            this.UserPwd.Text = "123456";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "确认密码";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "邮箱";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(445, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "用户名";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(445, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "密码";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(532, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 35);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "登陆";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UserPwd);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textPwd);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "登录与注册";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.TextBox textPwd;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox UserPwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ dengluzhucce/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace dengluzhucce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    //    private delegate DialogResult MessageBoxShow(string x);
+    //    private MessageBoxShow method;
+        private int box_show = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constr = @"server=DESKTOP-D6NGD8R\SQLEXPRESS;database=dlzc;uid=sa;pwd=741213;";
+            SqlConnection connection = new SqlConnection(constr);
+            connection.Open();
+            string comstr = "insert into dlzc values('" + textUser.Text + "','" + textPwd.Text + "','" + textEmail.Text + "')";
+            SqlCommand command = new SqlCommand(comstr, connection);
+            int count = command.ExecuteNonQuery();
+            if (count > 0)
+            {
+                MessageBox.Show("注册成功 !");
+            }
+            connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string constr = @"server=DESKTOP-D6NGD8R\SQLEXPRESS;database=dlzc;uid=sa;pwd=741213;";
+            SqlConnection connection = new SqlConnection(constr);
+            connection.Open();
+            string sql = "select * from dlzc";
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader dr = command.ExecuteReader();
+            bool cunzai = false;
+            while (dr.Read())
+            {
+                if ((string)dr["用户名"] == Username.Text)
+                {
+                    cunzai = true;
+                    if ((string)dr["密码"] == UserPwd.Text)
+                    {
+                        MessageBox.Show("登录成功");
+                    }
+                    else
+                        MessageBox.Show("密码不正确");
+                }
+            }
+            if (cunzai == false)
+                MessageBox.Show("用户不存在");
+            dr.Close();
+            connection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string constr = @"server=DESKTOP-D6NGD8R\SQLEXPRESS;database=dlzc;uid=sa;pwd=741213;";
+            SqlConnection connection = new SqlConnection(constr);
+            connection.Open();
+            string sql = "select * from dlzc";
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader dr = command.ExecuteReader();
+            while (dr.Read())
+            {
+                if ((string)dr["用户名"] == Username.Text)
+                {
+                    box_show = 1;
+                    if ((string)dr["密码"] == UserPwd.Text)
+                    {
+                        box_show = 2;   
+                    }
+                    else
+                        box_show = 3;
+                }
+            }
+            showbox();
+            dr.Close();
+            connection.Close();
+        }
+
+        private void showbox()
+        {
+            switch (box_show)
+            {
+                case 1:
+                    MessageBox.Show("用户不存在");
+                    break;
+                case 2:
+                    MessageBox.Show("登录成功");
+                    break;
+                case 3:
+                    MessageBox.Show("密码不正确");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
